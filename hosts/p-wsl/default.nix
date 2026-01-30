85bd66d2a1557b24d04c33b59a0780fb2b653d86
Hyunghwan Shin <hwan.shin@gmail.com>
2026-01-30 02:22:14 +0000

    add Korean fonts to p-wsl for Telegram on WSL

--- hosts/p-wsl/default.nix
+++ hosts/p-wsl/default.nix
@@ -10,4 +10,19 @@
   users.users.hwan = {
     extraGroups = ["dialout" "uucp"];
   };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+
+  fonts.fontconfig = {
+    defaultFonts = {
+      serif = ["Noto Serif CJK KR" "Noto Serif"];
+      sansSerif = ["Noto Sans CJK KR" "Noto Sans"];
+      monospace = ["Noto Sans Mono CJK KR" "Noto Sans Mono"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
 }
