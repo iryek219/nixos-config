8ea82ae4290973a3115e366011cd09b6a12e4477
Hyunghwan Shin <hwan.shin@gmail.com>
2026-02-02 08:02:58 +0000

    add pkgs.openssl to LD_LIBRARY_PATH and then nix fmt

--- outsiders/Rust/shell.nix
+++ outsiders/Rust/shell.nix
@@ -1,36 +1,35 @@
-{ pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
+{pkgs ? import <nixpkgs> {config.allowUnfree = true;}}: let
+  myVscode = pkgs.vscode-with-extensions.override {
+    vscode = pkgs.vscode;
 
-let
-  myVscode =
-    pkgs.vscode-with-extensions.override {
-      vscode = pkgs.vscode;
-
-      # nixpkgs에 이미 있는 확장은 이렇게
-      vscodeExtensions = with pkgs.vscode-extensions; [
-        rust-lang.rust-analyzer
-        tamasfe.even-better-toml
-        serayuzgur.crates
-        vadimcn.vscode-lldb
-      ];
-    };
+    # nixpkgs에 이미 있는 확장은 이렇게
+    vscodeExtensions = with pkgs.vscode-extensions; [
+      rust-lang.rust-analyzer
+      tamasfe.even-better-toml
+      serayuzgur.crates
+      vadimcn.vscode-lldb
+    ];
+  };
 in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rustup          # rustup만 두고 rustc/cargo는 rustup이 설치
-    #myVscode
-    #lldb
-    pkg-config      # (옵션) 네이티브 의존성 자주 쓰면
-    openssl         # (옵션) 예: openssl-sys
-    cmake clang     # (옵션) C/C++ 빌드 필요 시
-  ];
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      rustup # rustup만 두고 rustc/cargo는 rustup이 설치
+      #myVscode
+      #lldb
+      pkg-config # (옵션) 네이티브 의존성 자주 쓰면
+      openssl # (옵션) 예: openssl-sys
+      cmake
+      clang # (옵션) C/C++ 빌드 필요 시
+    ];
 
-  shellHook = ''
-    export PATH="$HOME/.cargo/bin:$PATH"
-    export CARGO_HOME="$HOME/.cargo"
-    export RUSTUP_HOME="$HOME/.rustup"
-    if ! command -v rustc >/dev/null; then
-      echo "[rustup] 최초 1회:  rustup default stable"
-    fi
-    echo ">>> Entered Rust dev shell."
-  '';
-}
+    shellHook = ''
+      export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.openssl]}:$LD_LIBRARY_PATH"
+      export PATH="$HOME/.cargo/bin:$PATH"
+      export CARGO_HOME="$HOME/.cargo"
+      export RUSTUP_HOME="$HOME/.rustup"
+      if ! command -v rustc >/dev/null; then
+        echo "[rustup] 최초 1회:  rustup default stable"
+      fi
+      echo ">>> Entered Rust dev shell."
+    '';
+  }
