756a70e3b818b48921a26c6c7ff58dcdf75fb13d
Hyunghwan Shin <hwan.shin@gmail.com>
2026-02-18 10:55:40 +0900

    Remove comments

--- home/openclaw.nix
+++ home/openclaw.nix
@@ -13,28 +13,6 @@
     # Path to managed documents directory
     documents = /home/hwan/code/openclaw-local/documents;
 
-    # config = {
-    #   gateway = {
-    #     mode = "local";
-    #     auth = {
-    #       # Gateway authentication token
-    #       token = "xcV3qOFmoXlqU8rsIgimxfMwImFF9X4z";
-    #     };
-    #   };
-    #
-    #   channels = {
-    #     telegram = {
-    #       # Path to Telegram bot token file
-    #       tokenFile = "/home/hwan/.secrets/telegram-bot-token";
-    #       # Your Telegram user ID from @userinfobot
-    #       allowFrom = [1078515864];
-    #       groups = {
-    #         "*" = {requireMention = true;};
-    #       };
-    #     };
-    #   };
-    # };
-
     instances.default = {
       enable = true;
       plugins = [];
