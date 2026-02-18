15655e6c2df4a9710dd3de587c1f003a7b75f96d
Hyunghwan Shin <hwan.shin@gmail.com>
2026-02-19 07:07:57 +0900

    Make Python/flake.nix to support both X86-64 and arm64

--- outsiders/Python/flake.nix
+++ outsiders/Python/flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Python dev environment";
   inputs = {
@@ -7,42 +6,48 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux"; 
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        # 1. We provide uv here
-        packages = [
-          pkgs.python312
-          pkgs.uv
-        ];
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            # 1. We provide uv here
+            packages = [
+              pkgs.python312
+              pkgs.uv
+            ];
 
-        # 2. We expose necessary C libraries to the environment
-        # This fixes issues where 'pip build' fails to find headers
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc
-          pkgs.zlib
-          # Add other libs here if a specific python package needs them (e.g. pkgs.glib)
-        ];
+            # 2. We expose necessary C libraries to the environment
+            # This fixes issues where 'pip build' fails to find headers
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.zlib
+              # Add other libs here if a specific python package needs them (e.g. pkgs.glib)
+            ];
 
-        # 3. Environment variables to help tools play nice
-        shellHook = ''
-          # Optional: helpful message
-          echo $(uv --version)
-          echo "Python $(python --version)"
+            # 3. Environment variables to help tools play nice
+            shellHook = ''
+              # Optional: helpful message
+              echo $(uv --version)
+              echo "Python $(python --version)"
 
-          export UV_PYTHON_DOWNLOADS=never
+              export UV_PYTHON_DOWNLOADS=never
 
-          echo 
-          echo "Important:"
-          echo "   UV venv should use the same python compiler as the one this flake declares"
-          echo "   This is to ensure that python packages and stdenv.cc.cc are from the same Nix pkgs."
-          echo "   The python downloaded from internet may not have been compiled against the same version of glibc or libstdc++so.6(pkgs.stdenv.cc.cc) from Nix pkgs"
-          echo "Create venv with"
-          echo "   uv venv --python \$(which python)"
-          echo
-        '';
-      };
+              echo 
+              echo "Important:"
+              echo "   UV venv should use the same python compiler as the one this flake declares"
+              echo "   This is to ensure that python packages and stdenv.cc.cc are from the same Nix pkgs."
+              echo "   The python downloaded from internet may not have been compiled against the same version of glibc or libstdc++so.6(pkgs.stdenv.cc.cc) from Nix pkgs"
+              echo "Create venv with"
+              echo "   uv venv --python \$(which python)"
+              echo
+            '';
+          };
+        });
     };
 }
